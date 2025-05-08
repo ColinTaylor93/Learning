2c461f61b567baf9a54765cc9fc408cd68fa3cda
colintaylor93 <colintaylor15@gmail.com>
2025-05-07 21:56:59 -0400

    Added endpoint to get a single note

--- fastAPI/app/main.py
+++ fastAPI/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from . import database, models, schemas
 
@@ -23,6 +23,14 @@ async def root():
 @app.get("/notes")
 def get_all_notes(db: Session = Depends(get_db)):
     return db.query(models.Note).all()
+
+@app.get("/notes/{note_id}")
+def get_single_note(note_id: int, db: Session = Depends(get_db)):
+    found_note = db.query(models.Note).filter(models.Note.id == note_id).first()
+    if not found_note:
+        raise HTTPException(status_code=404, detail="Note not found")
+    else:
+        return found_note
 
 @app.post("/notes")
 def create_note(note: schemas.NoteCreate, db: Session = Depends(get_db)):
